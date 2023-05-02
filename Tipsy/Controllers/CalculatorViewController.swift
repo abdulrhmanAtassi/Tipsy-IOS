bd717a41395e886a70b460453eb2d18ad517ab2d
abdalrhmanatass <65854575+abdalrhmanatass@users.noreply.github.com>
2023-05-02 19:45:15 +0300

    VC

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -9,24 +9,77 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
-    @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
- 
-
-    @IBAction func tipChanged(_ sender: Any) {
+    var splitNumber = 2
+    var bill = 0.0
+    var tip = 0
+    var billForPerson = 0.0
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        allOff()
+        billTextField.endEditing(true)
+        s(sender.currentTitle!)
+        sender.isSelected = true
+        
+        
     }
     
-    @IBAction func stepperValueChanged(_ sender: Any) {
+    @IBAction func stepperValueChanged(_ sender: UIStepper ) {
+        billTextField.endEditing(true)
+        splitNumberLabel.text = String(format: "%.f", sender.value)
+        splitNumber = Int(sender.value)
+        
     }
     
     
     @IBAction func calculatePressed(_ sender: Any) {
+        bill = Double( billTextField.text!) ?? 0.0
+        let percentage = calculatePercentage(value: bill,percentageVal: Double(tip))
+        let fullBill = bill + percentage
+        billForPerson = fullBill / Double(splitNumber)
+        self.performSegue(withIdentifier: "goToResult", sender: self)
     }
     
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "goToResult" {
+                let destinationVC = segue.destination as! ResultsViewController
+                destinationVC.billForPerson = billForPerson
+                destinationVC.tip = tip
+                destinationVC.splitNumber = splitNumber
+                
+            }
+        }
+    
+    
+    func allOff(){
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+    }
+    
+    func s(_ t:String){
+        switch t {
+        case "10%":
+            tip = 10
+        case "20%":
+            tip = 20
+        default:
+            tip = 0
+        }
+    }
+    
+    func calculatePercentage(value:Double,percentageVal:Double) -> Double{
+        if percentageVal != 0{
+            let val = value * percentageVal
+            return val / 100.0
+        }
+        return value
+    }
 }
 
--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -9,22 +9,32 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    
+    var splitNumber = 2
+    var billForPerson: Double?
+    var tip = 0
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        totalLabel.text = String(format: "0.2f", billForPerson!)
+        settingsLabel.text = "Split between \(splitNumber) people, with \(tip)% tip."
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func recalculatePressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
